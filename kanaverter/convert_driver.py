71afaefd8bd1f665880c16ac928e4c7738584195
Chris Loew <cloew123@gmail.com>
2020-11-13 14:57:50 -0700

    Added basic sentence parsing

--- kanaverter/convert_driver.py
+++ kanaverter/convert_driver.py
@@ -4,3 +4,21 @@ import pyperclip
 def run(args):
     """ Convert the kana sentence """
     print("Running...:", args)
+    
+    print("Split sentence:", split_sentence(' '.join(args)))
+
+def split_sentence(sentence):
+    """ Split the sentence into the different parts """
+    parts = []
+    part1, *rest = sentence.split('[', 1)
+    if part1 != '':
+        parts.append(part1)
+    if not rest:
+      return parts
+    part2, *rest = rest[0].split(']', 1)
+    if part2 != '':
+        parts.append(part2)
+    if not rest:
+      return parts
+    
+    return parts + split_sentence(rest[0])
