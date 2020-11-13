aecf90651c0df4919d152bec2a3141e05e9b4e46
Chris Loew <cloew123@gmail.com>
2020-11-13 15:19:31 -0700

    Added ability to parse out the kanji and kana based on the bracketed sentence

--- kanaverter/kana_sentence.py
+++ kanaverter/kana_sentence.py
@@ -8,13 +8,26 @@ def split_sentence(sentence):
         parts.append(KanaPart(part1))
     if not rest:
       return parts
-    part2, *rest = rest[0].split(']', 1)
-    if part2 != '':
-        parts.append(KanaPart(part2))
+    partWithKanji, *rest = rest[0].split(']', 1)
+    if partWithKanji != '':
+        parts.append(split_part_with_kanji(partWithKanji))
     if not rest:
       return parts
     
     return parts + split_sentence(rest[0])
+    
+def split_part_with_kanji(partWithKanji):
+    """ Splits out the aprt with Kanji """
+    indexToSplitAt = None
+    for i, character in enumerate(partWithKanji):
+        if not is_kanji(character):
+            indexToSplitAt = i
+            break
+    return KanaPart(kana=partWithKanji[i:], kanji=partWithKanji[:i])
+
+def is_kanji(character):
+    """ Return if the character is a kanji character """
+    return 0x4E00 <= ord(character) <= 0x9FBF
     
 class KanaSentence:
     """ Represents a sentence with kana and kanji """
