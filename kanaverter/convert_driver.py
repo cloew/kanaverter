585aba51f92173e2cfb6eb65f79fcf10554ec1e6
Chris Loew <cloew123@gmail.com>
2020-11-13 16:27:54 -0700

    Added argparser to allow the user to specify the output format

--- kanaverter/convert_driver.py
+++ kanaverter/convert_driver.py
@@ -6,10 +6,19 @@ import pyperclip
 
 def run(args):
     """ Convert the kana sentence """
-    parser = GroupedParser()
-    formatter = SimpleFormatter()
+    argparser = argparse.ArgumentParser(description="Convert between various formats of sentences with Kanji & Kana")
+    argparser.add_argument('sentence')
+    argparser.add_argument('--simple', '-s', action='store_true', help="Convert output to a simple format without readings for kanji")
+
+    parsed_args = argparser.parse_args(args)
+
+    formatter = SimpleFormatter() if parsed_args.simple else FuriganaFormatter()
     
-    sentence = parser.parse(' '.join(args))
+    parse_and_format(GroupedParser(), formatter, parsed_args.sentence)
+    
+def parse_and_format(parser, formatter, sentence_text):
+    """ Parse and format the sentence text with the given parser and formatter """
+    sentence = parser.parse(sentence_text)
     formatted_sentence = formatter.format(sentence)
     
     pyperclip.copy(formatted_sentence)
