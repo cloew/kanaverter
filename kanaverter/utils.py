fc5d12728eef738773bfa6de78565119e778be46
Chris Loew <cloew123@gmail.com>
2020-11-22 20:13:51 -0700

    Updated the script to handle 々

--- kanaverter/utils.py
+++ kanaverter/utils.py
@@ -1,4 +1,4 @@
 
 def is_kanji(character):
     """ Return if the character is a kanji character """
-    return 0x4E00 <= ord(character) <= 0x9FBF
+    return 0x4E00 <= ord(character) <= 0x9FBF or character == '々'
